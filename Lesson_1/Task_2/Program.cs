e4cdecfbf8c243dd297f09931c65ef21a9b1c98a
Evgeniy Gorbachov <zekagor@gmail.com>
2023-03-06 10:02:00 +0200

    First task(task2) was done, here you can write only integer number.

---
+++ Lesson_1/Task_2/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberAddedTen = number + 10;
+Console.WriteLine($"Your number plus ten: {numberAddedTen}");
