9cd63afdfc5e5ed2e8ef09463593feb5ccc8d977
Evgeniy Gorbachov <zekagor@gmail.com>
2023-03-06 11:24:02 +0200

    Task is done, program has some functions, which return strings or void.

---
+++ Lesson_1/Task_4/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.Write("Enter your first text: ");
+string string1 = Console.ReadLine();
+
+Console.Write("Enter your second text: ");
+string string2 = Console.ReadLine();
+
+int lengthString1 = string1.Length;
+int lengthString2 = string2.Length;
+
+if (lengthString1 > lengthString2)
+{
+    Console.WriteLine("First string is bigger then second: ");
+    Console.WriteLine(AddStrings(string1, string2));
+}
+
+else if (lengthString1 == lengthString2)
+{
+    Console.WriteLine("First and second strings are equal: ");
+    ReturnVoid();
+}
+
+else
+{
+    Console.WriteLine("Second string is bigger then first: ");
+    Console.WriteLine(SplitStrings(string1, string2));
+}
+
+string AddStrings(string string1, string string2)
+{
+    return string1 + string2;
+}
+
+string SplitStrings(string string1, string string2)
+{
+    string[] splitedStrings = string2.Split(string1[0]);
+    string result = string.Join("", splitedStrings);
+    return result;
+}
+
+void ReturnVoid()
+{
+    return;
+}
