22615f52874c28538a3f9fb04cebfae6e7eb5834
Evgeniy Gorbachov <zekagor@gmail.com>
2023-03-06 16:59:51 +0200

    I delete one variable.

--- Lesson_1/Task_2/Program.cs
+++ Lesson_1/Task_2/Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.Write("Enter your number: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int numberAddedTen = number + 10;
-Console.WriteLine($"Your number plus ten: {numberAddedTen}");
+
+Console.WriteLine($"Your number plus ten: {number + 10}");
