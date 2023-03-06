d2ee65cd70aab26d07c4efcf8eb6a865dc079695
Evgeniy Gorbachov <zekagor@gmail.com>
2023-03-06 10:47:50 +0200

    Task 3(task2) is done, here we can enter only integer numbers

---
+++ Lesson_1/Task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿List<int> array = new List<int>();
+for(int i = 0; i < 10; i++)
+{
+    Console.Write($"Enter {i + 1} number of array: ");
+    array.Add(Convert.ToInt32(Console.ReadLine()));
+}
+for(int i = 0; i < 10; i++)
+{
+    for(int j = 1 + i; j < 10; j++)
+    {
+        if (array[i] == array[j])
+            array.Add(array[i]);
+    }
+}
+for (int i = 0; i < array.Count; i++)
+{
+    Console.WriteLine($"{i+1} number of array = {array[i]}");
+}
