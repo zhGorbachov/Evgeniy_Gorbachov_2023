0e6fad06fb4b222abc8622b2fe6217873c2921d7
Evgeniy Gorbachov <zekagor@gmail.com>
2023-03-06 17:04:08 +0200

    Was deleted all function.

--- Lesson_1/Task_4/Program.cs
+++ Lesson_1/Task_4/Program.cs
@@ -10,34 +10,19 @@ int lengthString2 = string2.Length;
 if (lengthString1 > lengthString2)
 {
     Console.WriteLine("First string is bigger then second: ");
-    Console.WriteLine(AddStrings(string1, string2));
+    string result = string1 + " "  + string2;
+    Console.WriteLine(result);
 }
 
 else if (lengthString1 == lengthString2)
 {
     Console.WriteLine("First and second strings are equal: ");
-    ReturnVoid();
 }
 
 else
 {
     Console.WriteLine("Second string is bigger then first: ");
-    Console.WriteLine(SplitStrings(string1, string2));
-}
-
-string AddStrings(string string1, string string2)
-{
-    return string1 + string2;
-}
-
-string SplitStrings(string string1, string string2)
-{
     string[] splitedStrings = string2.Split(string1[0]);
     string result = string.Join("", splitedStrings);
-    return result;
-}
-
-void ReturnVoid()
-{
-    return;
+    Console.WriteLine(result);
 }
