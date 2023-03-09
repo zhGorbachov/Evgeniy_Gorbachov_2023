7f28ef4ef5cef26ee4ca6d7e7c731988e78800b1
Evgeniy Gorbachov <zekagor@gmail.com>
2023-03-09 13:20:05 +0200

    bug fixed: the last element of an array could not be duplicated.

--- Lesson_1/Task_3/Program.cs
+++ Lesson_1/Task_3/Program.cs
@@ -9,12 +9,13 @@ for(int i = 0; i < 10; i++)
 Console.Write("Enter a number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-for(int i = 0; i < 10; i++)
+for(int i = 0, max = 10; i < max; i++)
 {
     if (array[i] == number)
     {
         array.Insert(i + 1, number);
         i++;
+        max++;
     }
 }
 for (int i = 0; i < array.Count; i++)
