cad6353ffcbfbcb4de8dd3ce37436b5131f77b35
Evgeniy Gorbachov <zekagor@gmail.com>
2023-04-01 11:46:38 +0300

    Closed to change, but open to expansion.

---
+++ Lesson_4/Task_2/ProgrammistMagic.cs
@@ -0,0 +1,14 @@
+﻿namespace Open_Closed;
+
+public class ProgrammistMagic : MagicClass
+{
+    public override void CountYourMagic(int magic)
+    {
+        base.CountYourMagic(magic);
+        if(magic == 0)
+        {
+            Console.WriteLine("Wow you are a real programmer, you have the magic to break everything!");
+            return;
+        }
+    }
+}
