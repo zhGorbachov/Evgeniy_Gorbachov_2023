8fda94749ce2ec561ae9fddf378ef0f74252f684
Evgeniy Gorbachov <zekagor@gmail.com>
2023-04-03 12:45:16 +0300

    Added another class which working with list.

--- Lesson_4/Task_1/Customer.cs
+++ Lesson_4/Task_1/Customer.cs
@@ -5,42 +5,12 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Balance { get; set; }
-        
+
         public Customer(int id, string name, decimal balance)
         {
-            Id = id; 
-            Name = name; 
+            Id = id;
+            Name = name;
             Balance = balance;
-        }
-
-        public List<Customer> CustomersList = new List<Customer>()
-        {
-            new Customer(1, "Fikus", 0),
-            new Customer(2, "VHarbar", 100000)
-        };
-
-        public void PrintBalanceById(int id)
-        {
-            Console.WriteLine($"Your balance is: {GetBalanceById(id)}");
-        }
-        public decimal GetBalanceById(int id)
-        {
-            var customer = GetCustomerById(id);
-            return customer.Balance;
-        }
-        public Customer GetCustomerById(int id)
-        {
-            return CustomersList.FirstOrDefault(x => x.Id == id);
-        }
-        public void SaveToDatabase()
-        {
-            Console.WriteLine("Saved!");
-        }
-        public void UpdateBalanceCustomerById(int id, decimal newBalance)
-        {
-            var customer = GetCustomerById(id);
-            customer.Balance = newBalance;
-            SaveToDatabase();
         }
     }
 }
---
+++ Lesson_4/Task_1/WorkingWithCustomers.cs
@@ -0,0 +1,34 @@
+﻿namespace SingleResponsibility;
+
+public class WorkingWithCustomers
+{
+    public List<Customer> CustomersList = new List<Customer>()
+    {
+        new Customer(1, "Fikus", 0),
+        new Customer(2, "VHarbar", 100000)
+    };
+    
+    public void PrintBalanceById(int id)
+    {
+        Console.WriteLine($"Your balance is: {GetBalanceById(id)}");
+    }
+    public decimal GetBalanceById(int id)
+    {
+        var customer = GetCustomerById(id);
+        return customer.Balance;
+    }
+    public Customer GetCustomerById(int id)
+    {
+        return CustomersList.FirstOrDefault(x => x.Id == id);
+    }
+    public void SaveToDatabase()
+    {
+        Console.WriteLine("Saved!");
+    }
+    public void UpdateBalanceCustomerById(int id, decimal newBalance)
+    {
+        var customer = GetCustomerById(id);
+        customer.Balance = newBalance;
+        SaveToDatabase();
+    }
+}
