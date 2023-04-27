d255afc86ee7e43e55b4ec92fc743ecf32be0149
Evgeniy Gorbachov <zekagor@gmail.com>
2023-04-27 15:43:56 +0300

    Added new separate Calculator class.

---
+++ Lesson_6/Task/Calculator.cs
@@ -0,0 +1,31 @@
+﻿namespace Task;
+
+public class Calculator
+{
+    public delegate double Action(double firstNumber, double secondNumber);
+
+    public delegate void ActionHandler(string information);
+
+    public event ActionHandler? Calculation;
+    
+    // Function of calculator works
+    
+    public Action Sum = (firstNumber, secondNumber) => firstNumber + secondNumber;
+    
+    public Action Minus = (firstNumber, secondNumber) => firstNumber - secondNumber;
+        
+    public Action Multiply = (firstNumber, secondNumber) => firstNumber * secondNumber;
+
+    public Action Devision = (firstNumber, secondNumber) =>
+    {
+        if (secondNumber != 0)
+            return firstNumber / secondNumber;
+
+        throw new DivideByZeroException();
+    };
+            
+    public Action Exponentiation = (firstNumber, secondNumber) => Math.Pow(firstNumber, secondNumber);
+
+    public void Calculating(double firstNumber, double secondNumber, Action action) =>
+        Calculation?.Invoke($"First and second number gives {action(firstNumber, secondNumber)}");
+}
--- Lesson_6/Task/Program.cs
+++ Lesson_6/Task/Program.cs
@@ -1,77 +1,49 @@
-﻿class DelegatesTask
+﻿using Task;
+
+void ShowResult(string information)
 {
-    delegate double Action(double firstNumber, double secondNumber);
-
-    delegate void ActionHandler(string information);
-
-    static event ActionHandler? Calculation; 
-    static void Main(string[] args)
-    {
-        // Function of calculator works
-        Action Sum = (firstNumber, secondNumber) => firstNumber + secondNumber;
-
-
-        Action Minus = (firstNumber, secondNumber) => firstNumber - secondNumber;
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(information);
+}
         
-        Action Multiply = (firstNumber, secondNumber) => firstNumber * secondNumber;
+// User input
+Console.Write("Enter your action,\n" +
+              "it cans be: +, -, *, /, ^: ");
 
-        Action Devision = (firstNumber, secondNumber) =>
-        {
-            if (secondNumber != 0)
-                return firstNumber / secondNumber;
+char userAction = Char.Parse(Console.ReadLine());
 
-            throw new DivideByZeroException();
-        };
-            
-        Action Exponentiation = (firstNumber, secondNumber) => Math.Pow(firstNumber, secondNumber);
+Console.Write("Enter first number: ");
+float firstNumber = float.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+float secondNumber = float.Parse(Console.ReadLine());
 
-        void Calculating(double firstNumber, double secondNumber, Action action) =>
-        Calculation?.Invoke($"First and second number gives {action(firstNumber, secondNumber)}");
+var calculator = new Calculator();
 
-        void ShowResult(string information)
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(information);
-        }
-        
-        // User input
-        Console.Write("Enter your action,\n" +
-                      "it cans be: +, -, *, /, ^: ");
+// Switch
+calculator.Calculation += ShowResult;
 
-        char userAction = Char.Parse(Console.ReadLine());
-
-        Console.Write("Enter first number: ");
-        float firstNumber = float.Parse(Console.ReadLine());
-        Console.Write("Enter second number: ");
-        float secondNumber = float.Parse(Console.ReadLine());
-
-        // Switch
-        Calculation += ShowResult;
-
-        switch (userAction)
-        {
-            case '+':
-                Calculating(firstNumber, secondNumber, Sum);
+switch (userAction)
+{
+    case '+':
+        calculator.Calculating(firstNumber, secondNumber, calculator.Sum);
                 
-                break;
-            case '-':
-                Calculating(firstNumber, secondNumber, Minus);                
-                break;
-            case '*':
-                Calculating(firstNumber, secondNumber, Multiply);                
-                break;
-            case '/':
-                Calculating(firstNumber, secondNumber, Devision);
-                break;
-            case '^':
-                Calculating(firstNumber, secondNumber, Exponentiation);
-                break;
-            case 'e':
-                Console.WriteLine("\n\nProgram shuts down...");
-                break;
-            default:
-                Console.WriteLine("Wrong action, please read more carefully");
-                break;
-        }
-    }
+        break;
+    case '-':
+        calculator.Calculating(firstNumber, secondNumber, calculator.Minus);                
+        break;
+    case '*':
+        calculator.Calculating(firstNumber, secondNumber, calculator.Multiply);                
+        break;
+    case '/':
+        calculator.Calculating(firstNumber, secondNumber, calculator.Devision);
+        break;
+    case '^':
+        calculator.Calculating(firstNumber, secondNumber, calculator.Exponentiation);
+        break;
+    case 'e':
+        Console.WriteLine("\n\nProgram shuts down...");
+        break;
+    default:
+        Console.WriteLine("Wrong action, please read more carefully");
+        break;
 }
