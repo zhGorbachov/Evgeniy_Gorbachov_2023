41bac8183cc551fb347f67dfa1f4dc30a22cafb4
Evgeniy Gorbachov <zekagor@gmail.com>
2023-04-27 15:49:19 +0300

    Fast changes, all what was in program.cs moved to calculator.cs.

--- Lesson_6/Task/Calculator.cs
+++ Lesson_6/Task/Calculator.cs
@@ -28,4 +28,55 @@ public class Calculator
 
     public void Calculating(double firstNumber, double secondNumber, Action action) =>
         Calculation?.Invoke($"First and second number gives {action(firstNumber, secondNumber)}");
+    
+    public void ShowResult(string information)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(information);
+    }
+
+    public void StartCalculator()
+    {
+        // User input
+        Console.Write("Enter your action,\n" +
+                      "it cans be: +, -, *, /, ^: ");
+
+        char userAction = Char.Parse(Console.ReadLine());
+
+        Console.Write("Enter first number: ");
+        float firstNumber = float.Parse(Console.ReadLine());
+        Console.Write("Enter second number: ");
+        float secondNumber = float.Parse(Console.ReadLine());
+
+
+        // Switch
+        Calculation += ShowResult;
+
+        switch (userAction)
+        {
+            case '+':
+                Calculating(firstNumber, secondNumber, Sum);
+                
+                break;
+            case '-':
+                Calculating(firstNumber, secondNumber, Minus);                
+                break;
+            case '*':
+                Calculating(firstNumber, secondNumber, Multiply);                
+                break;
+            case '/':
+                Calculating(firstNumber, secondNumber, Devision);
+                break;
+            case '^':
+                Calculating(firstNumber, secondNumber, Exponentiation);
+                break;
+            case 'e':
+                Console.WriteLine("\n\nProgram shuts down...");
+                break;
+            default:
+                Console.WriteLine("Wrong action, please read more carefully");
+                break;
+        }
+    }
+    
 }
--- Lesson_6/Task/Program.cs
+++ Lesson_6/Task/Program.cs
@@ -1,49 +1,5 @@
 ﻿using Task;
 
-void ShowResult(string information)
-{
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine(information);
-}
-        
-// User input
-Console.Write("Enter your action,\n" +
-              "it cans be: +, -, *, /, ^: ");
-
-char userAction = Char.Parse(Console.ReadLine());
-
-Console.Write("Enter first number: ");
-float firstNumber = float.Parse(Console.ReadLine());
-Console.Write("Enter second number: ");
-float secondNumber = float.Parse(Console.ReadLine());
-
 var calculator = new Calculator();
 
-// Switch
-calculator.Calculation += ShowResult;
-
-switch (userAction)
-{
-    case '+':
-        calculator.Calculating(firstNumber, secondNumber, calculator.Sum);
-                
-        break;
-    case '-':
-        calculator.Calculating(firstNumber, secondNumber, calculator.Minus);                
-        break;
-    case '*':
-        calculator.Calculating(firstNumber, secondNumber, calculator.Multiply);                
-        break;
-    case '/':
-        calculator.Calculating(firstNumber, secondNumber, calculator.Devision);
-        break;
-    case '^':
-        calculator.Calculating(firstNumber, secondNumber, calculator.Exponentiation);
-        break;
-    case 'e':
-        Console.WriteLine("\n\nProgram shuts down...");
-        break;
-    default:
-        Console.WriteLine("Wrong action, please read more carefully");
-        break;
-}
+calculator.StartCalculator();
